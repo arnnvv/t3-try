ddf3217d3e529a3819d19b6986e4a3dcf179c15e
arnnvv <arnav.900@protonmail.com>
2024-06-14 13:24:06 +0530

    DBResolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,41 @@
-export default function HomePage(): JSX.Element {
-  const photoUrls: string[] = [
-    "https://www.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-1-5c6fe612590ec__700.jpg",
-    "https://www.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-18-5c6fe631f2eb3__700.jpg",
-    "https://static.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-12-5c6fe6271254d__700.jpg",
-    "https://static.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-7-5c6fe61d585f4__700.jpg",
-  ];
+import db from "~/server/db";
 
-  const photos = photoUrls.map(
-    (
-      url: string,
-      index: number,
-    ): {
-      id: string;
-      url: string;
-    } => ({
-      id: (index + 1).toString(),
-      url,
-    }),
-  );
+const photoUrls: string[] = [
+  "https://www.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-1-5c6fe612590ec__700.jpg",
+  "https://www.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-18-5c6fe631f2eb3__700.jpg",
+  "https://static.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-12-5c6fe6271254d__700.jpg",
+  "https://static.boredpanda.com/blog/wp-content/uploads/2019/02/fake-news-photos-viral-photoshop-7-5c6fe61d585f4__700.jpg",
+];
+
+const photos = photoUrls.map(
+  (
+    url: string,
+    index: number,
+  ): {
+    id: string;
+    url: string;
+  } => ({
+    id: (index + 1).toString(),
+    url,
+  }),
+);
+
+export default async function HomePage(): Promise<JSX.Element> {
+  const posts = await db.query.posts.findMany();
 
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map(
+          (post: {
+            id: number;
+            name: string | null;
+            createdAt: Date;
+            updatedAt: Date | null;
+          }): JSX.Element => (
+            <div key={post.id}>{post.name}</div>
+          ),
+        )}
         {[...photos, ...photos, ...photos, ...photos].map(
           (photo: { id: string; url: string }): JSX.Element => (
             <div key={photo.id} className="w-48">
