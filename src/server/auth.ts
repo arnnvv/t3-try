de9eafbb89389eacbfa54a312e41569ebfdba3b9
arnnvv <arnav.900@protonmail.com>
2024-06-15 15:53:57 +0530

    adapter

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -2,8 +2,12 @@ import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
 import { type NextAuthOptions } from "next-auth";
 import { env } from "~/env";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import db from "./db";
+import { type Adapter } from "next-auth/adapters";
 
 const authOptions: NextAuthOptions = {
+  adapter: DrizzleAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
       clientId: env.GOOGLE_CLIENT_ID,
