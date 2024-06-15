8c7a61b84cee2964a5c941b9221602bdb7f75f39
arnnvv <arnav.900@protonmail.com>
2024-06-15 17:16:56 +0530

    authAtLAst

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -1,10 +1,15 @@
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
-import { type NextAuthOptions } from "next-auth";
+import {
+  type Session,
+  getServerSession,
+  type NextAuthOptions,
+} from "next-auth";
 import { env } from "~/env";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import db from "./db";
 import { type Adapter } from "next-auth/adapters";
+import Credentials from "next-auth/providers/credentials";
 
 const authOptions: NextAuthOptions = {
   adapter: DrizzleAdapter(db) as Adapter,
@@ -17,8 +22,35 @@ const authOptions: NextAuthOptions = {
       clientId: env.GITHUB_CLIENT_ID,
       clientSecret: env.GITHUB_CLIENT_SECRET,
     }),
+    Credentials({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "text",
+          placeholder: "kaj@kjd.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "********",
+        },
+      },
+      async authorize() {
+        return null;
+      },
+    }),
   ],
   secret: env.NEXTAUTH_SECRET,
 };
+
+/**
+ * Wrapper for `getServerSession` so that you don't need to import the `authOptions` in every file.
+ *
+ * @see https://next-auth.js.org/configuration/nextjs
+ */
+export const getServerAuthSession = (): Promise<Session | null> =>
+  getServerSession(authOptions);
 
 export default authOptions;
