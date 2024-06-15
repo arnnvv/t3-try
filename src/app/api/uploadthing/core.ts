368e6a54fede0d342025114ecb1209c8c39ddfd3
arnnvv <arnav.900@protonmail.com>
2024-06-15 23:45:45 +0530

    userrelatedPics

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -32,6 +32,7 @@ export const ourFileRouter = {
       await db.insert(images).values({
         name: file.name,
         url: file.url,
+        userId: metadata.userId,
       });
       return { uploadedBy: metadata.userId };
     }),
