06c1e5f1d9fa53bb3da4d3f4c61564cbb7bad344
arnnvv <arnav.900@protonmail.com>
2024-06-16 14:33:50 +0530

    nextImage

--- src/app/_components/Images.tsx
+++ src/app/_components/Images.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import getImages from "~/server/queries";
 
 export const dynamic = "force-dynamic";
@@ -9,8 +10,14 @@ const Images = async (): Promise<JSX.Element> => {
     <div className="flex flex-wrap gap-4">
       {images.map(
         (photo: Image): JSX.Element => (
-          <div key={photo.id} className="flex w-48 flex-col">
-            <img src={photo.url} alt="image" />
+          <div key={photo.id} className="flex w-48 flex-col justify-center">
+            <Image
+              src={photo.url}
+              alt={photo.name}
+              style={{ objectFit: "contain" }}
+              width={480}
+              height={480}
+            />
             <p>{photo.name}</p>
           </div>
         ),
